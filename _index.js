ec8bf90a08233a8aabe58fa376a5467527046c85
Vidyadhar Thatte <vd_thatte@yahoo.com>
2015-06-05 23:29:54 -0400

    Fri Jun 5 11:29PM

--- _index.js
+++ _index.js
@@ -6,6 +6,14 @@ document.onscroll = function() {
 	console.log("scrolling");
 }
 
+var blog_button = document.getElementById("menu_li");
+var about_button = document.getElementById("menu_li");
+var contact_button = document.getElementById("menu_li");
+var portfolio_button = document.getElementById("menu_li");
+
+menu_button.onClick() = function(){
+	window.onscrollBy();
+}
 
 window.onload = function() {
 	//include all the javascript functions here.
